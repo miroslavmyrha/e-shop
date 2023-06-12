a14ef80583055e8f7320585f2105bb6bc93646eb
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-06-12 17:49:36 +0200

    added shop appbar

--- vue-shop/src/layouts/default/AppBar.vue
+++ vue-shop/src/layouts/default/AppBar.vue
@@ -1,13 +1,45 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+  <v-row>
+    <v-col
+      v-for="(bar, i) in bars"
+      :key="i"
+      cols="12"
+      sm="12"
+      md="12"
+      class="my-4"
+    >
+      <v-toolbar
+        :color="bar.class"
+        :dark="bar.dark"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Vue E-shop</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-cart-outline</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-col>
+  </v-row>
 </template>
-
-<script lang="ts" setup>
-  //
+<script>
+  export default {
+    data: () => ({
+      bars: [
+        { class: '' }
+      ],
+    }),
+  }
 </script>
+
+
+
+
+
+
