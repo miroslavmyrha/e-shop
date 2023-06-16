5418926ea76428bccac8212fba640266c7b149b7
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-06-16 16:26:50 +0200

    Update AppBar.vue - added test data attributes

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -11,17 +11,18 @@
       <v-toolbar
         :color="bar.class"
         :dark="bar.dark"
+        data-test="toolbar"
       >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Vue E-shop</v-toolbar-title>
+        <v-app-bar-nav-icon data-test="hamburger-menu"></v-app-bar-nav-icon>
+        <v-toolbar-title data-test="page-title">Vue E-shop</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
+        <v-btn icon data-test="search-button">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-        <v-btn icon>
+        <v-btn icon data-test="shopping-cart">
           <v-icon>mdi-cart-outline</v-icon>
         </v-btn>
-        <v-btn icon>
+        <v-btn icon data-test="options-extension">
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
       </v-toolbar>
