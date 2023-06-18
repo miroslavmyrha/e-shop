80eaa6e7a2d70714662974ed71d361c8fd0c7543
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-06-18 16:10:19 +0200

    test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -5,14 +5,17 @@ describe('template spec', {  defaultCommandTimeout: 10000 }, () => {
 
   it('passes', () => {
     cy.visit('/')
+    cy.visit('/')
   })
 
   it('hamburger test', () => {
+    cy.visit('/')
     cy.get('button[data-test="hamburger-menu"]').as('hamburgerMenu')
     cy.wait('@hamburgerMenu').should('exist').and('be.visible')
   })
 
   it('header test', () => {
+    cy.visit('/')
     cy.get('header').should('exist').and('be.visible')
   })
 })
