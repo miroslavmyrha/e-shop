aa2f53a21c26ad18909373192e6154c82316c661
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-06-18 14:48:19 +0200

    hamburger menu test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
   })
+
+  it('hamburger test', () => {
+    cy.get('[data-test="hamburger-menu"]').should('exist').and('be.visible')
+  })
 })
