baabf3c2968a9fa12e9f4ace7643149266bca66d
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-06-18 18:03:06 +0200

    added next tests

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', {  defaultCommandTimeout: 10000 }, () => {
-  it('visit', () => {
+describe('E2E tests: ', {  defaultCommandTimeout: 10000 }, () => {
+  it('Page can be loaded', () => {
     cy.visit('/')
   })
 
@@ -7,14 +7,30 @@ describe('template spec', {  defaultCommandTimeout: 10000 }, () => {
     beforeEach(() => {
       cy.visit('/')
       cy.viewport(1920, 1080)
-    }) 
+    })
+    
+    it('Toolbar should exist and be visible', () => {
+      cy.get('[data-test="toolbar"]').should('exist').and('be.visible')
+    })
 
-    it('hamburger test', () => {
+    it('Hamburger menu should exist and be visible', () => {
       cy.get('button[data-test="hamburger-menu"]').should('exist').and('be.visible')
     })
   
-    it('header test', () => {
-      cy.get('header').should('exist').and('be.visible')
+    it('Title has text: "Vue E-shop" and should exist and be visible', () => {
+      cy.get('[data-test="page-title"]').should('exist').and('be.visible')
+    })
+
+    it('Search button should exist and be visible', () => {
+      cy.get('[data-test="search-button"]').should('exist').and('be.visible')
+    })
+
+    it('Shopping cart should exist and be visible', () => {
+      cy.get('[data-test="shopping-cart"]').should('exist').and('be.visible')
+    })
+
+    it('Option extension should exist and be visible', () => {
+      cy.get('[data-test="options-extension"]').should('exist').and('be.visible')
     })
   })
 })
